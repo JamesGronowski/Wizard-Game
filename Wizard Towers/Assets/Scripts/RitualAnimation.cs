c8969aee366c54e22f9aa13d6dd59f82c0b66903
AlbeyAmakiir <albeyamakiir@gmail.com>
2016-01-30 10:22:10 +1100

    Actual commit for RitualAnimation.

---
+++ Wizard Towers/Assets/Scripts/RitualAnimation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RitualAnimation : MonoBehaviour {
+
+	public static void FireballAnimation(Player source) {
+		//Animate caster
+		//Create fireball
+		//source.transform.position
+	}
+}
---
+++ Wizard Towers/Assets/Scripts/RitualEffect.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RitualEffect : MonoBehaviour {
+
+	public static void FireballEffect(Player source) {
+		source.getTurn().target.addHealth(-10);
+	}
+}
