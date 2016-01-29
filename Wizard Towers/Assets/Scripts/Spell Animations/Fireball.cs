353d9c97de9fd78f2f54ff4e4370651915f9782b
Marcus <mdaygmdesigner@gmail.com>
2016-01-30 09:18:45 +1100

    Made animations script folder
    Made game Scene
    
    Started on fireball script

---
+++ Wizard Towers/Assets/Scripts/Spell Animations/Fireball.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireball : MonoBehaviour {
+
+	public float speed;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		//Instantiate(
+		
+	
+	}
+}
