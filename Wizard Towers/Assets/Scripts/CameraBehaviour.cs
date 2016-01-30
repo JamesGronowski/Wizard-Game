59c0acc5e9ed773f69a2470d401da5d7dbce3f15
JimmehG <j.gronowski@gmail.com>
2016-01-30 13:47:22 +1100

    Added Linearly interpolated camera

---
+++ Wizard Towers/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraBehaviour : MonoBehaviour {
+    private Dictionary<GameController.Phase, Vector3> CameraPositions = new Dictionary<GameController.Phase, Vector3>
+    {
+        {GameController.Phase.Results, new Vector3(0, 0, 15.8f) },
+        {GameController.Phase.Player1, new Vector3(-16.5f, 3.8f, 12) },
+        {GameController.Phase.Player2, new Vector3(16.5f, 3.8f, 12) }
+    };
+    public float speed;
+    private bool moving = false;
+    private Vector3 startingPos;
+    private Vector3 destination;
+    private float startTime;
+    private float journeyLength;
+
+    // Update is called once per frame
+    void Update() {
+        if (moving)
+        {
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            Vector3 currentPos = Vector3.Lerp(startingPos, destination, fracJourney);
+            transform.position = new Vector3(currentPos.x, currentPos.y, -10);
+            GetComponent<Camera>().orthographicSize = currentPos.z;
+        }
+    }
+
+    public void OnViewChange(GameController.Phase View)
+    {
+        startingPos = new Vector3(transform.position.x, transform.position.y, GetComponent<Camera>().orthographicSize);
+        destination = CameraPositions[View];
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startingPos, destination);
+        moving = true;
+    }
+}
--- Wizard Towers/Assets/Scripts/GameController.cs
+++ Wizard Towers/Assets/Scripts/GameController.cs
@@ -1,19 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class GameController : MonoBehaviour
 {
     private Phase currentPhase;
     public enum Phase {Results, Player1, Player2};
+    private Phase currentView;
 
+    public CameraBehaviour cam;
 
     // Use this for initialization
     void Start()
     {
         currentPhase = Phase.Player1;
         TriggerPhaseObjects();
+
+        currentView = Phase.Player1;
+        TriggerCameraView();
     }
+
+    
 
     // Update is called once per frame
     void Update()
@@ -33,11 +41,21 @@ public class GameController : MonoBehaviour
             currentPhase++;
         }
 
+        if (currentView == Phase.Player2)
+        {
+            currentView = Phase.Results;
+        }
+        else
+        {
+            currentView++;
+        }
+
         Debug.Log(currentPhase.ToString());
 		Debug.Log(Ritual.FIREBALL.Castable(new Rune[] {Rune.a}));
 		Debug.Log(Ritual.FIREBALL.Castable(new Rune[] {Rune.a, Rune.f}));
         
         TriggerPhaseObjects();
+        TriggerCameraView();
 
         if (currentPhase == Phase.Results) {
             RunResults();
@@ -60,6 +78,11 @@ public class GameController : MonoBehaviour
         {
             p.addRunes();
         }
+    }
+
+    void TriggerCameraView()
+    {
+        cam.OnViewChange(currentView);
     }
 
 }
